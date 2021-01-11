aada297197ccfa713d1116643f3868b6293050c5
kaylamuraoka <kmurs98@gmail.com>
2021-01-11 13:45:10 -1000

    create a seeds.sql file in the db folder with insert queries to populate the burgers table with at least three entries

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+/*
+
+To run this file, we do the following in our Terminal:
+
+1. Go to the directory of this sql file.
+
+2. Get into our mysql console.
+
+3. Run "source schema.sql"
+
+*/
+
+-- Specify the 'burgers_db' for use.
+USE burgers_db;
+
+-- Insert a set of records into the 'burgers' table
+INSERT INTO burgers (id, burger_name, devoured) VALUES ();
