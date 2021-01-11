73ed5e7203ff6ec6a8b0a8050c6b4754242c202e
kaylamuraoka <kmurs98@gmail.com>
2021-01-11 13:34:54 -1000

    create a seeds.sql file in the db folder with queries to create the burgers_db and table

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+/*
+
+To run this file, we do the following in our Terminal:
+
+1. Go to the directory of this sql file.
+
+2. Get into our mysql console.
+
+3. Run "source schema.sql"
+
+*/
+
+-- Create the database 'burgers_db' and specified it for use.
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
+
+-- Create the table 'burger'.
+CREATE TABLE burgers (
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(50) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY(id)
+);
