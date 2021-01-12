529b8663d7b0d0125dcb9fae3be69246d703d74e
kaylamuraoka <kmurs98@gmail.com>
2021-01-11 15:05:02 -1000

    add Object Relational Mapper js file that has the methods that will execute the necessary MySQL commands in the controllers

---
+++ config/orm.js
@@ -0,0 +1,90 @@
+// Import (require) connection.js into orm.js
+const connection = require("./connection");
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+
+// This 'printQuestionMarkshelper' function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+const printQuestionMarks = (num) => {
+  const arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+};
+
+// Helper function to convert object key/value pairs to SQL syntax
+const objToSql = (ob) => {
+  const arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (const key in ob) {
+    let value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = `'${value}'`;
+      }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(`${key}=${value}`);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+};
+
+// Create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+const orm = {
+  // The last variable cb represents the anonymous function being passed from server.js
+  selectAll(table, cb) {
+    const queryString = `SELECT * FROM ${table}`;
+    connection.query(queryString, (err, result) => {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertOne(table, cols, vals, cb) {
+    let queryString = `INSERT INTO ${table}`;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, (err, result) => {
+      if (err) throw err;
+
+      cb(result);
+    });
+  },
+  updateOne(table, objColVals, condition, cb) {
+    let queryString = `UPDATE ${table}`;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, (err, result) => {
+      if (err) throw err;
+
+      cb(result);
+    });
+  },
+};
+
+// Export the ORM object for the model burger.js
+module.exports = orm;
